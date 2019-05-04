ddcca7d1fda7482c1a15060cc32204f0b37a9dac
Camille Scholtz <camille@airmail.cc>
2019-05-04 13:13:51 +0200

    Fix config

--- config.go
+++ config.go
@@ -20,40 +20,38 @@ func parseConfig(c *caddy.Controller) (*config, error) {
 	cfg := &config{}
 
 	for c.Next() {
-		for c.NextBlock() {
-			switch c.Val() {
-			case "scope":
-				if !c.NextArg() {
-					return cfg, c.ArgErr()
-				}
-
-				cfg.Scope = c.Val()
-			case "destination":
-				if !c.NextArg() {
-					return cfg, c.ArgErr()
-				}
-
-				i, err := os.Stat(c.Val())
-				if err != nil {
-					return cfg, c.Err(err.Error())
-				}
-				if !i.IsDir() {
-					return cfg, c.ArgErr()
-				}
-
-				cfg.Dest = c.Val()
-			case "filename_length":
-				if !c.NextArg() {
-					return cfg, c.ArgErr()
-				}
-
-				l, err := strconv.ParseUint(c.Val(), 10, 32)
-				if err != nil {
-					return cfg, c.Err(err.Error())
-				}
-
-				cfg.Len = int(l)
+		switch c.Val() {
+		case "scope":
+			if !c.NextArg() {
+				return cfg, c.ArgErr()
 			}
+
+			cfg.Scope = c.Val()
+		case "destination":
+			if !c.NextArg() {
+				return cfg, c.ArgErr()
+			}
+
+			i, err := os.Stat(c.Val())
+			if err != nil {
+				return cfg, c.Err(err.Error())
+			}
+			if !i.IsDir() {
+				return cfg, c.ArgErr()
+			}
+
+			cfg.Dest = c.Val()
+		case "filename_length":
+			if !c.NextArg() {
+				return cfg, c.ArgErr()
+			}
+
+			l, err := strconv.ParseUint(c.Val(), 10, 32)
+			if err != nil {
+				return cfg, c.Err(err.Error())
+			}
+
+			cfg.Len = int(l)
 		}
 	}
 
